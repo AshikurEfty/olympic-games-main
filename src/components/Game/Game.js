36e57409e6fc99ec441dc56a9468908cc875daa6
AshikurEfty <ashikurefty713@gmai.com>
2022-10-16 13:44:48 +0600

    added react game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Game.css';
 
 const Game = (props) => {
+    const {handleAddToClick} = props;
     const {name, img, time} = props.game;
+    
     return (
         <div className='game'>
             <img src={img} alt="" />
             <h1>{name}</h1>
             <p>Time Required: {time}s</p>
-            <button className='select-btn'>Select Game</button>
+            <button onClick={()=>handleAddToClick(props.game)} className='select-btn'>Select Game</button>
         </div>
     );
 };
--- src/components/Sports/Sports.js
+++ src/components/Sports/Sports.js
@@ -5,11 +5,23 @@ import './Sports.css';
 const Sports = () => {
     const [games, setGames] = useState([]);
 
+    const [activity, setActivity] = useState([]);
+
+    // useEffect(() => {
+        
+    // },[])
+
     useEffect(() =>{
         fetch('olympic.json')
         .then(res => res.json())
         .then(data => setGames(data))
     },[]);
+
+    const handleAddToClick = (game)=>{
+        console.log(game);
+        const newActivity = [...activity, game];
+        setActivity(newActivity);
+    }
 
     return (
         <div className='sports-container'>
@@ -18,11 +30,12 @@ const Sports = () => {
                     games.map(game => <Game
                         key={game.id}
                         game={game}
+                        handleAddToClick={handleAddToClick}
                     ></Game>)
                 }
             </div>
             <div className="activity-container">
-                <h3>This is activity container</h3>
+                <h3>Activity items: {activity.length}</h3>
             </div>
         </div>
     );
